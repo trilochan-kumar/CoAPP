aab5efec7a4c0d145c57de95f5b04a1ef425cfd7
Trilochan Kumar <126813766+trilochan-kumar@users.noreply.github.com>
2024-03-27 16:17:00 +0530

    Update LibrariesCheck.py
    Changes to Libraries check as I am  going to use aiocoap instead of coapthon.

--- LibrariesCheck.py
+++ LibrariesCheck.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import QT_VERSION_STR, PYQT_VERSION_STR
 print("Qt: v", QT_VERSION_STR, "\tPyQt: v", PYQT_VERSION_STR)
 
-import coapthon
-print('imported coapthon too')
+import aiocoap
+print('imported aiocoap too')
